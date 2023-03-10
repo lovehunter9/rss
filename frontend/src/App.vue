c1dfa1b1b5edb2f62cebfbacd2498f26a00a9374
qq815776412 <815776412@qq.com>
2023-03-10 11:54:20 +0800

    test nginx agent path 'api'

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -2,10 +2,24 @@
   <router-view />
 </template>
 
-<script lang="ts">
+<script>
 import { defineComponent } from 'vue';
+import { useRssStore } from './stores/rss';
 
 export default defineComponent({
-  name: 'App'
+  name: 'App',
+  setup() {
+    console.log('login setup');
+    // const user =  useUserStore()
+    // user.generateUserMockData()
+
+    const rootStore = useRssStore();
+    const base_url = window.location.origin
+    console.log(base_url);
+    rootStore.setUrl(base_url);
+
+    console.log('login setup 2');
+    return {}
+  }
 })
 </script>
