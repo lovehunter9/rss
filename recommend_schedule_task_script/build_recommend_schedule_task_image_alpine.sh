e34787f977280da8dae54c036ab0b18ffb454bd1
Ubuntu <ubuntu@ip-172-31-1-188.ec2.internal>
2023-05-11 22:07:19 +0000

    remove redundant file

--- recommend_schedule_task_script/build_recommend_schedule_task_image_alpine.sh
+++
@@ -1,7 +0,0 @@
-SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
-RSS_DIR=$(dirname -- "$SCRIPT_DIR")
-DOCKER_FILE_PATH=$RSS_DIR/Dockerfile.recommend_schedule_task_alpine
-PREFIX=bytetrade
-docker build \
-    -f ${DOCKER_FILE_PATH} \
-    -t ${PREFIX}/recommend_schedule_task_alpine $RSS_DIR
