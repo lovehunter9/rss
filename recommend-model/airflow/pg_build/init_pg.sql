4b03a8c9a00432afb358cc890eb5f8398979f33a
wangzhong <zouwangzhong@bytetrade.io>
2023-05-22 13:20:32 +0000

    move recommend-model,recommend-model-sdk to recommend repo

--- recommend-model/airflow/pg_build/init_pg.sql
+++
@@ -1,3 +0,0 @@
-CREATE DATABASE airflow_db;
-CREATE USER airflow_user WITH PASSWORD 'airflow_pass';
-GRANT ALL PRIVILEGES ON DATABASE airflow_db TO airflow_user;
--- recommend-model/proto_class_unit_test/proto_convert_tool_unit_test.py
+++
@@ -1,8 +0,0 @@
-import unittest
-
-from proto_class.proto_convert_tool import ProtoConvertTool
-class ProtoConvertToolUnitTest(unittest.TestCase):
-    def test_convert(self):
-        # python  -m unittest proto_convert_tool_unit_test.ProtoConvertToolUnitTest.test_convert
-        tool = ProtoConvertTool()
-        tool.convert("embedding.proto")
