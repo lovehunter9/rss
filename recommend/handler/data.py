34fcf90cfd68492bd1b50d1ac9b1b2fe9a88305c
simon <ljx1680535@163.com>
2023-07-04 22:04:08 +0800

    remove image url

--- recommend/handler/data.py
+++ recommend/handler/data.py
@@ -98,11 +98,8 @@ class DataHandler:
                         'author': current_articles['author'],
                         'content': current_articles['content'],
                         'full_content': current_articles['full_text'],
-                        'title': current_articles['title'],
-                        'image_url': ''
+                        'title': current_articles['title']
                     }
-                    if 'image_url' in current_articles:
-                        article['image_url'] = current_articles['image_url']
                     article_list.append(article)
             if len(article_list) > 0:
                 tool.batch_insert_recommend_entries(article_list)
