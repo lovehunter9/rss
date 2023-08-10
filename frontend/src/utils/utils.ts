8066ec8da3e2c3208e056f37227762dc9d2921b3
icebergtsn <zyh2433219116@gmail.com>
2023-08-10 13:19:19 +0800

    format html content

--- frontend/src/utils/utils.ts
+++ frontend/src/utils/utils.ts
@@ -86,18 +86,26 @@ export const getPastTime = (stamp1: Date, stamp2: Date) => {
 
 
 export const formatContentHtml = (htmlContent: string) => {
-  let newContent = htmlContent.replace(/<img[^>]*>/gi, function (match) {
-    match = match.replace(/style="[^"]+"/gi, '').replace(/style='[^']+'/gi, '');
-    match = match.replace(/width="[^"]+"/gi, '').replace(/width='[^']+'/gi, '');
-    match = match.replace(/height="[^"]+"/gi, '').replace(/height='[^']+'/gi, '');
-    return match;
-  });
-  newContent = newContent.replace(/style="[^"]+"/gi, function (match) {
-    match = match.replace(/width:[^;]+;/gi, 'max-width:100%;').replace(/width:[^;]+;/gi, 'max-width:100%;');
-    return match;
-  });
-  newContent = newContent.replace(/<br[^>]*\/>/gi, '');
-  newContent = newContent.replace(/\<img/gi, '<img style="max-width:100%;height:auto;"');
+  // let newContent = htmlContent.replace(/<img[^>]*>/gi, function (match) {
+  //   match = match.replace(/style="[^"]+"/gi, '').replace(/style='[^']+'/gi, '');
+  //   match = match.replace(/width="[^"]+"/gi, '').replace(/width='[^']+'/gi, '');
+  //   match = match.replace(/height="[^"]+"/gi, '').replace(/height='[^']+'/gi, '');
+  //   return match;
+  // });
+  // newContent = newContent.replace(/style="[^"]+"/gi, function (match) {
+  //   match = match.replace(/width:[^;]+;/gi, 'max-width:100%;').replace(/width:[^;]+;/gi, 'max-width:100%;');
+  //   return match;
+  // });
+  // newContent = newContent.replace(/<br[^>]*\/>/gi, '');
+  // newContent = newContent.replace(/<p[^>]*\/>/gi, '');
+  // newContent = newContent.replace(/\<img/gi, '<img style="max-width:100%;height:auto;"');
+
+  const processedHtml1 = htmlContent.replace(/<p(.*?)>/gi, '<p$1 style="margin: 0.5em 0;">');
+  // Process <img> tags
+  const processedHtml2 = processedHtml1.replace(/<img(.*?)width=["']([^"']*)["'](.*?)>/gi, '<img$1style="max-width:100%;height:auto;" width="$2"$3>');
+  // Process <br> tags
+  const newContent = processedHtml2.replace(/<br\/?>/gi, '<br style="clear:both;">');
+
   return newContent;
 }
 
